5a43ff131df855f8d49db68f118b5ffa36b7bf1e
Mattt Zmuda <mattt@me.com>
2025-12-19 03:18:17 -0800

    Remove comments from Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 6.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -13,7 +12,6 @@ let package = Package(
         .visionOS(.v2),
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ConfigurationTOML",
             targets: ["ConfigurationTOML"]
@@ -24,8 +22,6 @@ let package = Package(
         .package(url: "https://github.com/mattt/swift-toml.git", from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "ConfigurationTOML",
             dependencies: [
